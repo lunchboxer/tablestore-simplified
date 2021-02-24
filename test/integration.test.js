adf8dbe3504e1fb75a12ca8de5a8c99bd795a02f
James Smith <metal.lunchbox@gmail.com>
2021-02-24 22:12:51 +0800

    fix integration tests for server race conditions

--- test/integration.test.js
+++ test/integration.test.js
@@ -4,91 +4,96 @@ const data = require('../index')
 
 const { listTables } = require('../src/database/list-tables')
 
-let animal = {}
-let animals = []
+// Nothing that relies on being up to date with the action preceeding it can be garanteed to work
+// if we add three rows and delete one row and want the row count to come back as exactly one
+// then we may fail unless there is a very slow connection.
+// the sequence on this end is reliable though
 
 test('database client should initialize and return table names without error', async t => {
+  t.plan(1)
+
   const tables = await listTables()
   t.ok(Array.isArray(tables), 'listTables returned an array')
-  t.end()
 })
 
-test('get and destroy all items', async t => {
-  const allAnimals = await data.get({ table: 'animals' })
-  const result = await data.destroy(allAnimals)
-  t.ok(result === undefined, 'destroy returns undefined')
-  const animalCount = await data.count({ table: 'animals' })
-  t.ok(animalCount === 0, 'count of items in animals table is 0')
-  t.end()
-})
+const animalsToDelete = []
 
 test('set function inserts individual item', async t => {
+  t.plan(4)
   const input = { table: 'animals', name: 'wally', type: 'walrus' }
-  animal = await data.set(input)
+  const animal = await data.set(input)
+  animalsToDelete.push(animal)
   const { key, ...attributes } = animal
   t.deepEqual(attributes, input, 'record matches input')
   t.ok(key, 'item given a key')
-  const hasOnlySafeCharacters = /^[\w -]+$/g.test(key)
-  t.ok(hasOnlySafeCharacters, 'key is string of safe characters')
+  const hasonlysafecharacters = /^[\w -]+$/g.test(key)
+  t.ok(hasonlysafecharacters, 'key is string of safe characters')
   t.ok(key.length === 12, 'key is 12 characters long')
-  t.end()
 })
 
 test('set function inserts batch items', async t => {
+  t.plan(2)
   const inputs = [
     { table: 'animals', name: 'polly', type: 'parrot' },
     { table: 'animals', name: 'kelly', type: 'kangaroo' },
   ]
-  animals = await data.set(inputs)
+  const animals = await data.set(inputs)
+  animalsToDelete.push(...animals)
   t.ok(Array.isArray(animals), 'returns an array')
-  const noKeys = inputs.map(a => {
+  const nokeys = inputs.map(a => {
     const { key, ...rest } = a
     return rest
   })
-  t.deepEqual(noKeys, inputs, 'return matches inputs')
-  t.end()
+  t.deepEqual(nokeys, inputs, 'return matches inputs')
 })
 
 test('get function retrieves individual item', async t => {
-  const key = animal.key
-  const retrievedAnimal = await data.get({ table: 'animals', key })
-  t.ok(retrievedAnimal.key, 'an item was returned')
-  t.end()
+  t.plan(1)
+  const key = animalsToDelete[0].key
+  const retrievedanimal = await data.get({ table: 'animals', key })
+  t.ok(retrievedanimal.key, 'an item was returned')
 })
 
-test('get function returns null when item doesnt exist', async t => {
-  const retrievedAnimal = await data.get({
+test('get function returns undefined when item doesnt exist', async t => {
+  t.plan(1)
+  const retrievedanimal = await data.get({
     table: 'animals',
     key: 'ahen0w8vn-92',
   })
   // begin/data actually returns null
-  t.ok(retrievedAnimal === undefined, 'returned undefined')
-  t.end()
+  t.ok(retrievedanimal === undefined, 'returned undefined')
 })
 
 test('get function retrieves whole table', async t => {
-  const allAnimals = await data.get({ table: 'animals' })
-  t.ok(Array.isArray(allAnimals), 'an array was returned')
-  t.ok(allAnimals.length === 3, 'returned 3 items')
+  t.plan(2)
+  const allanimals2 = await data.get({ table: 'animals' })
+  t.ok(Array.isArray(allanimals2), 'an array was returned')
   const count = await data.count({ table: 'animals' })
-  t.ok(count === 3, 'data.count returned 3')
-  t.end()
+  t.ok(Number.isInteger(count), 'data.count returned an integer')
 })
 
 test('get function returns empty array on multiple nonexistant items', async t => {
-  const allVehicles = await data.get({ table: 'vehicles' })
-  t.ok(Array.isArray(allVehicles), 'returns array')
-  t.ok(allVehicles.length === 0, 'array is empty')
-  t.end()
+  t.plan(2)
+  const allvehicles = await data.get({ table: 'vehicles' })
+  t.ok(Array.isArray(allvehicles), 'returns array')
+  t.ok(allvehicles.length === 0, 'array is empty')
 })
 
 test('calling with [], undefined, or {} handled', async t => {
+  t.plan(1)
   try {
-    await data.get([])
+    const response = await data.get([])
+    t.ok(
+      Array.isArray(response) && response.length === 0,
+      'get([]) returned []',
+    )
   } catch (error) {
     t.error(error)
   }
-  t.end()
 })
 
-data.destroy([animal, ...animals])
+test('clean up', async t => {
+  t.plan(1)
+  await data.destroy(animalsToDelete)
+  t.ok(true)
+})
